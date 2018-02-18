1bca2ed862ba245d94d6c9d433ae0d85bf9eeea9
Egor Ponomarev <egor@ponomarev.pp.ru>
2018-02-18 21:43:43 +0300

    rest

--- api.js
+++ api.js
@@ -28,8 +28,8 @@ data_base_api.get('/users/' || '/users', (req, res) => {
   });
 });
 
-data_base_api.put('/users/add/:name', (req, res) => {
-  var user = new User({ name: req.params.name })
+data_base_api.put('/users/add', (req, res) => { // /users/add?name=name
+  var user = new User({ 'name': req.query.name })
   user.save((err) => {
     if (err) return console.error(err);
     res.send(`Пользователь ${user.name} успешно создан. Ваш id: ${user._id}`);
@@ -38,10 +38,10 @@ data_base_api.put('/users/add/:name', (req, res) => {
 
 
 
-data_base_api.post('/users/trans/:id/name/:name', (req, res) => {
-  User.findById(req.params.id, (err, doc) => {
+data_base_api.post('/users/trans', (req, res) => { // /users/trans?id=id&name=name
+  User.findById(req.query.id, (err, doc) => {
     if (err) console.error(err);
-    doc.name = req.params.name;
+    doc.name = req.query.name;
     doc.save((err) => {
       if (err) return console.error(err);
       res.send(`Имя пользователя ${doc._id} успешно изменено на ${doc.name}.`);
@@ -49,36 +49,36 @@ data_base_api.post('/users/trans/:id/name/:name', (req, res) => {
   })
 });
 
-data_base_api.delete('/users/delete/:id', (req, res) => {
-  User.findByIdAndRemove(req.params.id, (err) => {
+data_base_api.delete('/users/delete', (req, res) => { // /users/delete?id=id
+  User.findByIdAndRemove(req.query.id, (err) => {
     if (err) return console.error(err);
-    res.send(`Пользователя ${doc._id} успешно удален.`);
+    res.send(`Пользователь успешно удален.`);
   })
 });
 
-data_base_api.get('/task' || '/task/', (req, res) => {
+data_base_api.get('/tasks' || '/tasks/', (req, res) => {
   Task.find((err, data) => {
     if (err) return console.error(err)
     res.send(data);
   });
 });
 
-data_base_api.get('/task/title/:title' || '/task/desc/:desc', (req, res) => {
-  if (req.params.title) {
-    Task.find({ title: req.params.title }, (err, data) => {
+data_base_api.get('/task/title' || '/task/desc', (req, res) => { // /task/title?title=title || task/desc?desc=desc
+  if (req.query.title) {
+    Task.find({ title: req.query.title }, (err, data) => {
       if (err) return console.error(err);
       res.send(data)
     })
   }
-  if (req.params.desc) {
-    Task.find({ description: req.params.desc }, (err, data) => {
+  if (req.query.desc) {
+    Task.find({ description: req.query.desc }, (err, data) => {
       if (err) return console.error(err);
       res.send(data);
     })
   }
 });
 
-data_base_api.put('/task/add/:title/desc/:desc', (req, res) => {
+data_base_api.put('/tasks/add', (req, res) => { // /tasks/add?title=title&desc=desc
   var task = new Task({ title: req.params.title, description: req.params.desc });
   task.save((err) => {
     if (err) return console.error(err);
@@ -86,10 +86,11 @@ data_base_api.put('/task/add/:title/desc/:desc', (req, res) => {
   })
 });
 
-data_base_api.post('/task/trans/:title/open/', (req, res) => {
-  Task.findOne({ title: req.params.title }, (err, data) => {
+data_base_api.post('/task/:id', (req, res) => { // /task/:id?open= true||false
+  Task.findById(req.params.id, (err, data) => {
     if (err) return console.error(err);
-    data.open = true;
+    if (req.query.open === 'true') data.open = true;
+    if (req.query.open === 'false') data.open = false;
     data.save((err) => {
       if (err) console.error(err);
       res.send('Задание открыто.');
@@ -97,26 +98,27 @@ data_base_api.post('/task/trans/:title/open/', (req, res) => {
   })
 });
 
-data_base_api.post('/task/trans/:title/close/', (req, res) => {
-  Task.findOne({ title: req.params.title }, (err, data) => {
+data_base_api.post('/task/:id/update', (req, res) => { // /task/:id/update?title=title&desc=desc
+  Task.findById(req.params.id, (err, data) => {
     if (err) return console.error(err);
-    data.open = false;
+    data.title = req.query.title;
+    data.description = req.query.desc;
     data.save((err) => {
-      if (err) return console.error(err);
-      res.send('Задание закрыто.');
+      if (err) console.error(err);
+      res.send('Задание обновлено.');
     })
   })
 });
 
-data_base_api.post('/task/adduser/:user/title/:title', (req, res) => {
-  User.findOne({ name: req.params.user }, (err, user) => {
+data_base_api.post('/task/:id/adduser', (req, res) => { // /task/:title/adduser?user=name
+  Task.findById(req.params.id, (err, task) => {
     if (err) return console.error(err);
-    Task.findOne({ title: req.params.title }, (err, data) => {
+    User.findOne({ name: req.query.user }, (err, user) => {
       if (err) return console.error(err);
-      data.user = user;
-      data.save((err) => {
+      task.user = user;
+      task.save((err) => {
         if (err) return console.error(err);
-        res.send(`Задание ${data.title} делегировано на пользователя ${data.user.name}`)
+        res.send(`Задание ${task.title} делегировано на пользователя ${data.user.name}`)
       });
     })
   });
